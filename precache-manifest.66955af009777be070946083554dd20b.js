b6dd52070e2701c21cee3766da97464476b87578
Bright Kanyange <brightkanyange@outlook.com>
2020-10-23 13:44:11 +0300

    Updates

---
+++ precache-manifest.66955af009777be070946083554dd20b.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58d7889b2d082d95d04b6abf0c28099e",
+    "url": "%20//brightkan.github.io/Portfolio-2020/index.html"
+  },
+  {
+    "revision": "2dadb3b917854033ad25",
+    "url": "%20//brightkan.github.io/Portfolio-2020/static/css/2.d2ca2446.chunk.css"
+  },
+  {
+    "revision": "da83025451698346cea8",
+    "url": "%20//brightkan.github.io/Portfolio-2020/static/css/main.6b92aa24.chunk.css"
+  },
+  {
+    "revision": "2dadb3b917854033ad25",
+    "url": "%20//brightkan.github.io/Portfolio-2020/static/js/2.96371a1f.chunk.js"
+  },
+  {
+    "revision": "da83025451698346cea8",
+    "url": "%20//brightkan.github.io/Portfolio-2020/static/js/main.af0980c4.chunk.js"
+  },
+  {
+    "revision": "9d77a823378e17ccf3e9",
+    "url": "%20//brightkan.github.io/Portfolio-2020/static/js/runtime-main.4f28609b.js"
+  },
+  {
+    "revision": "05beb7e8fba51b75514c537aa76ed8f9",
+    "url": "%20//brightkan.github.io/Portfolio-2020/static/media/brightkan.05beb7e8.png"
+  },
+  {
+    "revision": "3fe4a27b10a9094da4164c1d998a8bd5",
+    "url": "%20//brightkan.github.io/Portfolio-2020/static/media/brightkan_logo.3fe4a27b.png"
+  },
+  {
+    "revision": "ddf38a8f4321ffdaac27f2608781e7d8",
+    "url": "%20//brightkan.github.io/Portfolio-2020/static/media/cv.ddf38a8f.png"
+  },
+  {
+    "revision": "ddf38a8f4321ffdaac27f2608781e7d8",
+    "url": "%20//brightkan.github.io/Portfolio-2020/static/media/cv1.ddf38a8f.png"
+  },
+  {
+    "revision": "ddf38a8f4321ffdaac27f2608781e7d8",
+    "url": "%20//brightkan.github.io/Portfolio-2020/static/media/cv2.ddf38a8f.png"
+  },
+  {
+    "revision": "ddf38a8f4321ffdaac27f2608781e7d8",
+    "url": "%20//brightkan.github.io/Portfolio-2020/static/media/cv3.ddf38a8f.png"
+  },
+  {
+    "revision": "ddf38a8f4321ffdaac27f2608781e7d8",
+    "url": "%20//brightkan.github.io/Portfolio-2020/static/media/cv4.ddf38a8f.png"
+  },
+  {
+    "revision": "c31b5069f157b82943809ab2ecc38ef8",
+    "url": "%20//brightkan.github.io/Portfolio-2020/static/media/git.c31b5069.svg"
+  }
+]);
